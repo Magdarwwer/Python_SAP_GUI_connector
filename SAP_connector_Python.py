256f9157dcbec0d9e12526501f7f167322b8d1d1
magda <magdamoraga14@gmail.com>
2025-11-23 10:11:39 +0100

    added function for opening SAP

--- SAP_connector_Python.py
+++ SAP_connector_Python.py
@@ -3,3 +3,9 @@ import time
 import pyautogui
 import subprocess
 
+def open_SAP():
+    #Start SAP Logon
+    sap_gui_path = r"C:\Program Files\SAP\FrontEnd\SAPGUI\saplogon.exe" #copy path from your saplogon.exe location 
+    subprocess.Popen(sap_gui_path)
+    time.sleep(10)
+    
